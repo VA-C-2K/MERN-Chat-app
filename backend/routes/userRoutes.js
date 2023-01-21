a45dbb71b011720888a29956664653c59d51fa61
Viraj Agharkar <viraj@quolam.co>
2023-01-22 01:15:00 +0530

    API creation for chats

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, authUser, allUsers } = require("../controllers/userController");
+const { registerUser, authUser, allUsers } = require("../controllers/userControllers");
 const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router()
