050cb137013f6df848fafa58a548e6e042edc2ed
Viraj Agharkar <viraj@quolam.co>
2023-02-25 20:27:27 +0530

    Changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,19 +6,29 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("API is running");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV === "production"){
+    console.log("here")
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+    });
+}else{
+    app.get('/', (req, res) => {
+        res.send("API is running");
+    });
+}
 
 // Error Handling middlewares
 app.use(notFound);
